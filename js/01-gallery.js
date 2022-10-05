8d5fbbd7e89d75ee85299c8cc4af7f502ada087c
Larysa Holtseva <holtseva@seznam.cz>
2022-10-05 12:50:20 +0200

    add gallery1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,69 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+const gallaryContainer = document.querySelector(".gallery");
+
+function createGallaryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    /></a></div>`;
+    })
+    .join("");
+}
+
+gallaryContainer.addEventListener("click", onGallaryImageElClick);
+
+const gallaryItemsMarkup = createGallaryMarkup(galleryItems);
+
+gallaryContainer.innerHTML = gallaryItemsMarkup;
+
+function onGallaryImageElClick(event) {
+  const isGallaryImageEl = event.target.classList.contains("gallery__image");
+
+  if (!isGallaryImageEl) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", closeModal);
+      },
+
+      onClose: () => {
+        window.removeEventListener("keydown", closeModal);
+      },
+    }
+  );
+
+  instance.show();
+
+ 
+  blockPageRefresh(event);
+
+  function closeModal(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
+
+function blockPageRefresh(event) {
+  event.preventDefault();
+}
+
+
+
+
+
+
